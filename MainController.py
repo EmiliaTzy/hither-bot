c850485be433005a5af7b29e5ea0530effa5df76
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-09-11 05:44:22 +0200

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -337,6 +337,36 @@ def command_dead(update, context):
             msg = context.bot.send_message(cid, game.print_claims(), parse_mode="HTML")
             MsgController.msg[cid] = Message(cid, msg)
 
+def _start_webhook(self, listen, port, url_path, cert, key, bootstrap_retries, clean,
+                       webhook_url, allowed_updates):
+        self.logger.debug('Updater thread started')
+        use_ssl = cert is not None and key is not None
+        if not url_path.startswith('/'):
+            url_path = '/{0}'.format(url_path)
+
+        # Create and start server
+        self.httpd = WebhookServer((listen, port), WebhookHandler, self.update_queue, url_path,
+                                   self.bot)
+            
+        if use_ssl:
+            self._check_ssl_cert(cert, key)
+            
+            # DO NOT CHANGE: Only set webhook if SSL is handled by library
+            if not webhook_url:
+                webhook_url = self._gen_webhook_url(listen, port, url_path)
+
+            self._bootstrap(
+                max_retries=bootstrap_retries,
+                clean=clean,
+                webhook_url=webhook_url,
+                cert=open(cert, 'rb'),
+                allowed_updates=allowed_updates)
+        elif clean:
+            self.logger.warning("cleaning updates is not supported if "
+                                "SSL-termination happens elsewhere; skipping")
+
+        self.httpd.serve_forever(poll_interval=1)
+
 def main():
     GamesController.init()
     MsgController.init()
